007cad1f7846361a47691320bb08d63c75696c68
albertobenincasa <benincasa.alberto@gmail.com>
2020-09-24 15:09:23 +0200

    update doc2txt module

--- doc2txt.py
+++ doc2txt.py
@@ -1,6 +1,14 @@
 import os
 import sys
 import re
+import pdb
+
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+
+from urlextract import URLExtract
+
+from docx import Document
 
 try:
     import textract
@@ -16,10 +24,10 @@ def doc2txt(argv):
 
 def convert_to_txt(input_arg):
     if os.path.isfile(input_arg):
-        do_conversion(input_arg)
+        return do_conversion(input_arg)
     elif os.path.isdir(input_arg):
         for file in os.listdir(input_arg):
-            do_conversion(file)
+            return do_conversion(file)
     else:
         print('The file %s not exists.' % (input_arg))
         sys.exit(2)
@@ -28,94 +36,121 @@ def do_conversion(file):
     file_name, file_extension = os.path.splitext(file)
 
     if file_extension in white_list_extension_files:
-        text_bytes = textract.process(file, extension=file_extension)
-        text = text_bytes.decode('utf-8')
+
+        if file_extension == ".docx":
+            remove_header_footer(file)
+
+        text_bytes = textract.process(file, encoding='utf-8', extension=file_extension)
+        text = text_bytes.decode('utf-8')#.encode('utf-8')
 
         f = open(file_name + '.txt', 'w')
-        f.write(text)
+
+        if file_extension == ".pdf":
+            text = purge_index(text, file)
+
+        data = purge_urls(text, file_name)
+
+        # remove • unicode and substitute with -
+        data = re.sub('\u2022', '-', data)   
+        f.write(data)
         f.close()
+        return data
     else:
         print('This file extension is not supported. These are the ones supported: ')
         for ext in white_list_extension_files:
             print("%s\n" % (ext))
         sys.exit(2)
 
-def text2str(path, delimiter='.'):
-    file = open(path, 'r')
-    unparsed_info = file.read()
+def to_list(data):
     element_list = [] # Make an empty list
 
-    for elements in unparsed_info.split(delimiter):
-        e = elements.strip(delimiter)
-        if e != '':
-		    
-            element_list.append(e.replace('\n','')) #Append to list
+    for element in re.split('[.\n]', data):
+        stripped_element = element.strip()
+        if stripped_element != '':	    
+            element_list.append(stripped_element) #Append to list the striped element
     
     return element_list
 
-def normalization(path, delimiter='.'):
+def normalization(text, delimiter='.'):
     '''
     Each line is a complete phrase
     '''
-    file_input = open(path, 'r')
+    
+    unparsed_info = text
 
-    #print(file_input)
-    file_name, file_extension = os.path.splitext(os.path.basename(path))
-
-    unparsed_info = file_input.read()
-
-    file_output = open(file_name + "_nomalized" + file_extension, 'w')
-    #print(unparsed_info)
     bc_text = ' '.join(unparsed_info.split('\n'))
     
-    sentenceSplit = filter(None, bc_text.split("."))
+    sentenceSplit = bc_text.split(".")
     
-    for s in sentenceSplit :
-        #print(s)
-        #print(s.strip() + ".")
-        file_output.write(s.strip() + ".\n")
+    datas = ''
+    for s in sentenceSplit:
+        clean_s = re.sub('\s\s+', ' ', s)
+        if len(clean_s.strip()) > 0 and any(c.isalpha() for c in clean_s):
+            datas = datas + clean_s.strip() + ".\n"
+        
+
+    #file_output.write(datas)
+    
+    return datas
 
 def Find(string): 
-    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    url = re.findall(regex,string)       
-    return [x[0] for x in url]
+    extractor = URLExtract()
+    urls = extractor.find_urls(string)
+    return urls
 
-def purge_urls(path):
-    file_name, file_extension = os.path.splitext(os.path.basename(path))
+def purge_urls(text, file_name):
+    file_index = open(file_name + '_urls.txt', 'w')
 
-    file_input = open(path, 'r')
-
-    file_output = open(file_name + '_purged.txt', 'w')
-    file_index = open(file_name + '_index.txt', 'w')
-
-    unparsed_info = file_input.read().replace('-\n', '')
-    #print(unparsed_info)
+    unparsed_info = text.replace('-\n', '')
     index_count = 1
 
     urls = Find(unparsed_info)
+    
+    if len(urls) != 0:
+        for url in urls:
+            unparsed_info = re.sub(url, '[URL_'+str(index_count)+']', unparsed_info)
+            unparsed_info = unparsed_info + '\n'
+            file_index.write('[URL_'+str(index_count)+']' + '-' + url + '\n')
+            index_count = index_count + 1
 
-    for element in unparsed_info.splitlines():
-        urls = Find(element)
-        if len(urls) != 0:
-            for url in urls:
-                element = element.replace(url, str(index_count), 1)
-                file_index.write(str(index_count) + ' - ' + url + '\n')
-                index_count = index_count + 1
+    unparsed_info = re.sub(r'^\d{1,}\.[\s][a-zA-Z]*', '', unparsed_info)
 
-        char_presence = re.search('[a-zA-Z]', element)
-        chapter_present = re.search(r'^\d{1,}\.', element) 
-
-        if char_presence and not chapter_present:
-            file_output.write(element + '\n')
-
-
-    file_output.close()
-    file_input.close()
     file_index.close()
+    return normalization(unparsed_info)
 
-    normalization(file_name + '_purged.txt')
+def purge_index(data, file):
 
-if __name__ == "__main__":
-    #doc2txt(sys.argv[1:])
-    #normalization(sys.argv[1])
-    purge_urls(sys.argv[1])
+    titles = []
+
+    datas = ''
+
+    fp = open(file, 'rb')
+    parser = PDFParser(fp)
+    document = PDFDocument(parser)
+    # Get the outlines of the document.
+    outlines = document.get_outlines()
+
+    for (level,title,dest,a,se) in outlines:
+        #titles.append(''.join([i for i in title if not i.isdigit() and i != '.']).strip())
+        titles.append(title.strip())
+
+    bc_text = ' '.join(data.split('\n'))
+    
+    #sentenceSplit = bc_text.split(".")
+
+    for title in titles:
+        if re.search(title, bc_text, re.IGNORECASE):
+            bc_text = re.sub(title, '', bc_text, flags=re.IGNORECASE)
+
+    return bc_text
+
+def remove_header_footer(file):
+    
+    document = Document(file)
+
+    ''' rimuoviamo header & footer '''
+    for i in range(len(document.sections)):
+        document.sections[i].header.is_linked_to_previous = True
+        document.sections[i].footer.is_linked_to_previous = True
+
+    document.save(os.path.splitext(file)[0] + '.docx') 
